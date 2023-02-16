7723560a2c373313261b05a8d38f3f3376c41296
KMART <93807261+KMArtwork@users.noreply.github.com>
2023-02-15 17:50:35 -0800

    complete lab03, sans stretch goal

--- src/Main.js
+++ src/Main.js
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import Image from 'react-bootstrap/Image'
-import data from './data.json';
 import HornedBeast from './HornedBeast';
+import SelectedBeast from './SelectedBeast';
 
-class Main extends React.Component {
-    constructor(props){
-        super(props);
-        this.beasts = data;
+// class Main extends React.Component {
+//     constructor(props){
+//         super(props);
+//         this.beasts = this.props.data;
+//     };
+
+    function Main(props) {
+
+    const [showModal, setShowModal] = useState(false);
+    const [selectedCard, setSelectedCard] = useState({});
+
+    const handleCardClick = (cardData) => {
+        setSelectedCard(cardData);
+        // console.log('test');
+        // console.log(cardData);
+        openModal();
     }
 
-    render() {
+    const openModal = () => {
+        setShowModal(true);
+        console.log(selectedCard);
+    }
+
+    const closeModal = () => {
+        setShowModal(false);
+    }
+
         return (
             <main>
-                {this.beasts.map(beast => {
-                    return <HornedBeast title={beast.title} imageUrl={beast.image_url} description={beast.description} />
+                {props.data.map(beast => {
+                    return <HornedBeast beast={beast} title={beast.title} imageUrl={beast.image_url} description={beast.description} openModal={openModal} handleCardClick={handleCardClick} />
                 })}
+                <SelectedBeast 
+                show={showModal} 
+                handleClose={closeModal} 
+                title={selectedCard.title} 
+                img={selectedCard.image_url}
+                description={selectedCard.description}
+                horns={selectedCard.horns}
+                ></SelectedBeast>
             </main>
         )
     }
-}
 
 export default Main;
